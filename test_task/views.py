06c4c4a1fa52f84b98b2350932dff93ec1d879a0
LexSlus <sloboda276@gmail.com>
2022-08-07 15:06:25 +0300

    django ready project

--- test_task/views.py
+++ test_task/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from forms import UserLogInForm
 from django.contrib.auth.views import LoginView
 from api import Keitaro
-
+from djangoTestTask.settings import api_key, url_clicks, url_conversions
 
 # Create your views here.
 
@@ -19,15 +19,13 @@ class LogInUser(LoginView):
 
 @login_required
 def home(request):
-    api_key = '7b88e90dfad2f86bf250b0ac388176ec'
-    url_clicks = 'http://136.244.93.168/admin_api/v1/clicks/log'
-    url_conversions = 'http://136.244.93.168/admin_api/v1/conversions/log'
+
 
     total_clicks = Keitaro(api_key, url_clicks, limit=10).get_exact()
     total_conversions = Keitaro(api_key, url_conversions, limit=10).get_exact()
 
     sub_id = Keitaro(api_key=api_key,
-                     url='http://136.244.93.168/admin_api/v1/clicks/log', limit=10).get_sub()
+                     url=url_clicks, limit=10).get_sub()
 
     context = {'clicks': total_clicks[0], 'conversions': total_conversions, 'sub_id_6': sub_id}
     return render(request, 'home.html', context)
